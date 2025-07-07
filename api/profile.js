dbb7f324983dce34c053645ea682c41671c2e8f3
BSupercharged <derekwright79@gmail.com>
2025-07-07 11:15:20 +0200

    Update profile.js

--- api/profile.js
+++ api/profile.js
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
       return res.status(401).json({ success: false, error: "No access token found" });
     }
 
-    const response = await fetch("https://api.prod.whoop.com/v1/user", {
+    const response = await fetch("https://api.prod.whoop.com/oauth/user/profile", {
       method: "GET",
       headers: {
         Authorization: `Bearer ${latestToken.access_token}`,
