c41a94ad7a411158b5dfe545dd4b1c1a5ff7283d
BSupercharged <derekwright79@gmail.com>
2025-07-08 08:30:30 +0200

    Update login.js

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,14 +1,12 @@
 // /api/login.js
-
 export default async function handler(req, res) {
   const { user } = req.query;
   if (!user) {
-    return res.status(400).json({ error: "Missing (phone) number" });
+    return res.status(400).json({ error: "Missing user (phone) number" });
   }
-
-  // Use only last 9 digits
   const phone = user.replace(/[^\d]/g, '').slice(-9) || "000000000";
-  const state = phone;
+  const state = phone; // <------ ONLY the phone number, no "="
+
   const redirectUri = encodeURIComponent(process.env.WHOOP_REDIRECT_URI);
   const clientId = process.env.WHOOP_CLIENT_ID;
 
