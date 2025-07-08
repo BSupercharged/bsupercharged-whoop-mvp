c4921ce8902d968609ffc7b74cca415b73886663
BSupercharged <derekwright79@gmail.com>
2025-07-08 12:08:44 +0200

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -1,4 +1,19 @@
-// /pages/api/login.js
-export default function LoginPage() {
-  return <div>Login with WHOOP</div>;
+import Twilio from "twilio";
+
+export default async function handler(req, res) {
+  const { From, Body } = req.body;
+
+  // Add your user logic here later
+  try {
+    const client = Twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+    await client.messages.create({
+      from: process.env.TWILIO_WHATSAPP_NUMBER,
+      to: From,
+      body: `PONG TEST: ${Body}`,
+    });
+    console.log("Replied to WhatsApp:", From, Body);
+  } catch (e) {
+    console.error("[Twilio send error]", e);
+  }
+  res.status(200).send("pong sent");
 }
